ba5547dd1254dd5d8e7850ffb4bdb387b66d1419
高兴02 <gaoxing02@qianxin.com>
2021-12-10 14:45:02 +0800

    fix: 修复单个文件无法识别的问题

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ yarn add global unit2px
 $ unit2px [options]
 
 Options:
-  -d <dir...>         directory to be convert (default: [__dirname])
+  -d <dir...>         directory to be convert (default: ['.'])
   -t <type...>        file types to be convert (default: ['.html', '.css'])
   -w <width>          client width (default: 375)
   -h <height>         client height (default: 812)
--- bin/index.js
+++ bin/index.js
@@ -7,7 +7,7 @@ const program = new commander.Command();
 program
   .version(require('../package.json').version)
   .name('unit2px')
-  .option('-d, --dir <dir...>', 'directory to be convert (default: [__dirname])')
+  .option('-d, --dir <dir...>', `directory to be convert (default: ['.'])`)
   .option('-t, --type <type...>', `file types to be convert (default: ['.html', '.css'])`)
   .option('-w, --width <width>', 'client width (default: 375)')
   .option('-h, --height <height>', 'client height (default: 812)')
